8a70efbf84a0345acec98f7ebc624e9439893ee5
TomozQ <inf.4clover@gmail.com>
2020-07-15 14:59:56 +0900

    create_informDB

---
+++ db/migrate/20200714091944_create_informs.rb
@@ -0,0 +1,10 @@
+class CreateInforms < ActiveRecord::Migration[6.0]
+  def change
+    create_table :informs do |t|
+      t.string :content
+      t.references :store, foreign_key: true
+      t.references :user, foreign_key: true
+      t.timestamps
+    end
+  end
+end
