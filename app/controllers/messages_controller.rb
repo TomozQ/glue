6d96e10c9c8b8137903f9e3b04f07711f5c54e8f
TomozQ <inf.4clover@gmail.com>
2020-07-16 14:54:19 +0900

    edit_messagesview, edit_path

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -4,7 +4,6 @@ class MessagesController < ApplicationController
   def index
     @message = Message.new
     @messages = @group.messages.includes(:user).order("created_at DESC")
-    redirect_to group_path(params[:group_id])
   end
 
   def create
