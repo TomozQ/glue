101642721a445fc5451b2df8daf479b3cd601d62
TomozQ <inf.4clover@gmail.com>
2020-07-17 14:08:44 +0900

    add_ajax_to_message, edit_message#index_for_storeCreate_display

---
+++ app/assets/javascripts/message.js
@@ -0,0 +1,75 @@
+$(function(){
+  function buildHTML(message){
+    if (message.image) {
+      let html = `
+                    <div class="show-lists__chat-main__message-lists__contents__content">
+                      <div class="show-lists__chat-main__message-lists__contents__content__left">
+                        <div class="show-lists__chat-main__message-lists__contents__content__left__name">
+                          ${message.user_name}
+                        </div>
+                        <div class="show-lists__chat-main__message-lists__contents__content__left__text">
+                          ${message.content}
+                          <img class=".show-lists__chat-main__message-lists__contents__content__left__text__img" src="${message.image}">
+                        </div>
+                      </div>
+                      <div class="show-lists__chat-main__message-lists__contents__content__right">
+                        <div class="show-lists__chat-main__message-lists__contents__content__right__time-stamp">
+                          ${message.created_at}
+                        </div>
+                      </div>
+                    </div>
+                  `
+      return html;
+    } else {
+      let html = `
+                      <div class="show-lists__chat-main__message-lists__contents__content">
+                        <div class="show-lists__chat-main__message-lists__contents__content__left">
+                          <div class="show-lists__chat-main__message-lists__contents__content__left__name">
+                            ${message.user_name}
+                          </div>
+                          <div class="show-lists__chat-main__message-lists__contents__content__left__text">
+                            ${message.content}
+                          </div>
+                        </div>
+                        <div class="show-lists__chat-main__message-lists__contents__content__right">
+                          <div class="show-lists__chat-main__message-lists__contents__content__right__time-stamp">
+                            ${message.created_at}
+                          </div>
+                        </div>
+                      </div>
+                  `
+      return html
+    }
+}
+  $(function(){
+    $('.chat-main__message-form__Form').on("submit", function(e){
+      
+      e.preventDefault();
+      let formData = new FormData(this);
+      console.log(formData)
+      let url = $(this).attr('action');
+
+      $.ajax({
+        url: url,  //同期通信でいう『パス』
+        type: 'POST',  //同期通信でいう『HTTPメソッド』
+        data: formData,  
+        dataType: 'json',
+        processData: false,
+        contentType: false
+      })
+
+    .done(function(data){
+      let html = buildHTML(data);
+      $('.show-lists__chat-main__message-lists__contents').append(html);
+      $('form')[0].reset();
+      $('.show-lists__chat-main__message-lists').animate({ scrollTop: $('.show-lists__chat-main__message-lists')[0].scrollHeight});
+      $('.chat-main__message-form__Form__btn-send').prop('disabled', false);
+    })
+    .fail(function() {
+      alert("メッセージ送信に失敗しました");
+      $('.chat-main__message-form__Form__btn-send').prop('disabled', false);
+  });
+  })
+
+})
+});
